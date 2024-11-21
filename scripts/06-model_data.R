77652c51975ebca78161b677bcc80a63b7f43186
Dezhen Chen <dezhenchenpeter@gmail.com>
2024-11-21 03:04:11 -0500

    update model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -7,31 +7,39 @@
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
+install.packages("loo")
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
+library(car)
+library(brms)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data_train <- read_parquet("data/02-analysis_data/train_data.parquet")
 
-### Model data ####
-first_model <-
+model <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    formula = respondent_understood_binary ~ interview_conduct_method + partner_interference +
+      child_interference + parent_interference + relative_interference +
+      non_relative_interference + interviewer_age + interviewer_gender +
+      as.factor(country) + question_clarification + respondent_reluctant + respondent_tried_best,
+    data = analysis_data_train,
+    family = binomial(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
 
+vif(model)
+summary(model)
+
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  model,
+  file = "models/model.rds"
 )
 
 
